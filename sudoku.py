7633daa80d08eeabd4041a3986b157a521eed7f7
shiver <shivermail@gmail.com>
2009-09-21 23:08:22 +1200

    Implemented solver for 'Hidden Singles'

--- sudoku.py
+++ sudoku.py
@@ -8,10 +8,11 @@ BOARD_DIMENSIONS = 9
 # [1,2,3,4,5,6,7,8,9]
 VALID_VALUES = range(1, BOARD_DIMENSIONS + 1)
 BLANK_BOARD = [[0 for x in xrange(BOARD_DIMENSIONS)] for y in xrange(BOARD_DIMENSIONS)]
-EXAMPLE_STRING = '1.......2.9.4...5...6...7...5.9.3.......7.......85..4.7.....6...3...9.8...2.....1'
+HARD_STRING = '1.......2.9.4...5...6...7...5.9.3.......7.......85..4.7.....6...3...9.8...2.....1'
+EASY_STRING = '79....3.......69..8...3..76.....5..2..54187..4..7.....61..9...8..23.......9....54'
 
 
-DEBUG=True
+DEBUG=False
 
 class InvalidBoard(Exception):
     def __init__(self, board):
@@ -35,29 +36,36 @@ def createBoard(availableValues):
     return board
 
 def getAvailableValues(board, x, y):
-    available = []
+    if DEBUG:
+        print('(' + str(x) + ', ' + str(y) + ')')
 
+    available = []
     used = []
 
     row = getRow(board, y)
-    if DEBUG: displayRow(row)
+    if DEBUG:
+        print('Row:')
+        displayRow(row)
     for v in row:
         used.append(v)
 
     column = getColumn(board, x)
-    if DEBUG: displayColumn(column)
+    if DEBUG:
+        print('Column:')
+        displayColumn(column)
     for v in column:
         used.append(v)
         
     block = getBlock(board, x, y)
-    if DEBUG: displayBlock(block)
+    if DEBUG:
+        print('Block:')
+        displayBlock(block)
     for v in block:
         used.append(v)
 
     for v in VALID_VALUES:
         if not str(v) in used:
-            available.append(v)
-
+            available.append(str(v))
 
     if DEBUG:
         print('Available:')
@@ -68,16 +76,24 @@ def getAvailableValues(board, x, y):
 def getRow(board, rowIndex):
     return board[rowIndex]
 
+def setRow(board, rowIndex, row):
+    board[rowIndex] = row
+
 def getColumn(board, columnIndex):
     column = []
-    for row in xrange(BOARD_DIMENSIONS):
-        column.append(board[row][columnIndex])
+    for rowIndex in xrange(BOARD_DIMENSIONS):
+        column.append(board[rowIndex][columnIndex])
         
     return column
 
+def setColumn(board, columnIndex, column):
+    for rowIndex in xrange(BOARD_DIMENSIONS):
+        board[rowIndex][columnIndex] = column[rowIndex]
+
+    return board
+
 def getBlock(board, rowIndex, columnIndex):
-    startRowIndex = (rowIndex / (BOARD_DIMENSIONS / 3)) * (BOARD_DIMENSIONS / 3)
-    startColumnIndex = (columnIndex / (BOARD_DIMENSIONS / 3)) * (BOARD_DIMENSIONS / 3)
+    startRowIndex, startColumnIndex = getBlockStart(board, rowIndex, columnIndex)
 
     block = []
     for y in xrange(startColumnIndex, startColumnIndex + 3):
@@ -86,9 +102,27 @@ def getBlock(board, rowIndex, columnIndex):
 
     return block
 
+def setBlock(board, rowIndex, columnIndex, block):
+    startRowIndex, startColumnIndex = getBlockStart(board, rowIndex, columnIndex)
+
+    index = 0
+    for y in xrange(startColumnIndex, startColumnIndex + 3):
+        for x in xrange(startRowIndex, startRowIndex + 3):
+            board[y][x] = block[index]
+            index += 1
+
+    return block
+
+def getBlockStart(board, rowIndex, columnIndex):
+    startRowIndex = (rowIndex / (BOARD_DIMENSIONS / 3)) * (BOARD_DIMENSIONS / 3)
+    startColumnIndex = (columnIndex / (BOARD_DIMENSIONS / 3)) * (BOARD_DIMENSIONS / 3)
+
+    return startRowIndex, startColumnIndex
+
 def stringToBoard(boardString):
     dimension = math.sqrt(len(boardString))
     if dimension == int(dimension):
+        dimension = int(dimension)
         board = []
         index = 0
         for y in xrange(dimension):
@@ -104,17 +138,91 @@ def stringToBoard(boardString):
         return board
 
 def solveBoard(board):
+    unassigned = -1
+    while unassigned != 0:
+        if unassigned == -1:
+            unassigned = 0
+            
+        availableMap, unassigned = getAvailableMap(board)
+        assigned = assignSingles(board, availableMap)
+        availableMap, unassigned = getAvailableMap(board)
+        eliminateHiddenSingles(board, availableMap)
+        assigned += assignSingles(board, availableMap)
+        availableMap, unassigned = getAvailableMap(board)
+
+        print(str(assigned) + ' assigned\n')
+        if assigned == 0:
+            break
+
+    if DEBUG:
+        print(availableMap + '\n')
+
+    return board
+
+def getAvailableMap(board):
     dimension = len(board)
-    
+
     availableMap = []
+    unassigned = 0
     for y in xrange(dimension):
+        row = []
         for x in xrange(dimension):
             availableValues = []
             if board[y][x] in [0, '.']:
                 availableValues = getAvailableValues(board, x, y)
-            availableMap.append(availableValues)
+                unassigned += 1
+            row.append(availableValues)
+        availableMap.append(row)
+        
+    return availableMap, unassigned
 
-    print(availableMap)
+
+def assignSingles(board, availableMap):
+    dimension = len(board)
+
+    assigned = 0
+    for y in xrange(dimension):
+        for x in xrange(dimension):
+            if len(availableMap[y][x]) == 1:
+                board[y][x] = availableMap[y][x][0]
+                assigned += 1
+                # TODO: add DEBUG
+                print(availableMap[y][x][0] \
+                      + ' set at (' + str(x) \
+                      + ', ' + str(y) + ')')
+
+    if DEBUG:
+        print(str(assigned) + ' assigned\n')
+
+    return assigned
+
+def eliminateHiddenSingles(board, availableMap):
+    dimension = len(availableMap)
+
+    assigned = 0
+    for y in xrange(dimension):
+        column = getColumn(availableMap, y)
+        for x in xrange(dimension):
+            # Row
+            row = getRow(availableMap, x)
+            combined = []
+            for r in row:
+                combined.extend(r)
+
+            unique = []
+            for v in VALID_VALUES:
+                if (combined.count(str(v))) == 1:
+                    unique.append(str(v))
+
+            for u in unique:
+                for i in xrange(dimension):
+                    if u in row[i]:
+                        row[i] = [u]
+
+            setRow(availableMap, x, row)
+                
+            block = getBlock(availableMap, x, y)
+            
 
 def displayBoard(board):
     for row in board:
@@ -158,6 +266,7 @@ def generate():
             pass
 
 #generate()
-board = stringToBoard(EXAMPLE_STRING)
+board = stringToBoard(EASY_STRING)
 displayBoard(board)
 solveBoard(board)
+displayBoard(board)
