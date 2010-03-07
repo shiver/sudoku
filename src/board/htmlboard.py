5dee09f249eb71bafbcd02152a48b0c1fcc6df1d
Rob <robert@vally.co.za>
2010-03-08 09:26:25 +1300

    Another email address change to the README. Some display tweaks for the HTMLBoard.

--- src/board/htmlboard.py
+++ src/board/htmlboard.py
@@ -31,87 +31,35 @@ class HTMLBoard(Board):
         """
         
         cellString = """<div class="cell">"""
-        replacement = dict(x1='', x2='', x3='', x4='', x5='', x6='', x7='',
-                           x8='', x9='')
+        replacement = dict(x1='&nbsp;', x2='&nbsp;', x3='&nbsp;', x4='&nbsp;', 
+                           x5='&nbsp;', x6='&nbsp;', x7='&nbsp;', x8='&nbsp;', 
+                           x9='&nbsp;')
         
         if isinstance(value, ListType):
             # A list of values indicates we are dealing with an AvailableMap
-            
-            if len(value) <= 4:
-                # With only 4 values or less, we place values at each of the 
-                # four corners, starting with the top left.
-                # 
-                # E.g:
-                # 1.2
-                # ...
-                # 3.4
-                
-                cellString += """<div class="insidecell">
-                                    <span>$x1</span>
-                                    <span>&nbsp;</span>
-                                    <span>$x2</span>
-                                </div>
-                                <div class="insidecell">
-                                &nbsp;
-                                </div>
-                                <div class="insidecell">
-                                    <span>$x3</span>
-                                    <span>&nbsp;</span>
-                                    <span>$x4</span>
-                                </div>"""                     
-            elif len(value) <= 6:
-                # With 5 or 6 values we place values in two columns of 3.
-                #
-                # E.g:
-                # 1.2
-                # 3.4
-                # 5.6
-                
-                cellString += """<div class="insidecell">
-                                    <span>$x1</span>
-                                    <span>&nbsp;</span>
-                                    <span>$x2</span>
-                                </div>
-                                <div class="insidecell">
-                                    <span>$x3</span>
-                                    <span>&nbsp;</span>
-                                    <span>$x4</span>
-                                </div>
-                                <div class="insidecell">
-                                    <span>$x5</span>
-                                    <span>&nbsp;</span>
-                                    <span>$x6</span>
-                                </div>""" 
-            elif len(value) > 6:
-                # More than 6 values and we use all available spaces in the cell
-                #
-                # E.g:
-                # 123
-                # 456
-                # 789
-                cellString += """<div class="insidecell">
-                                    <span>$x1</span>
-                                    <span>$x2</span>
-                                    <span>$x3</span>
-                                    
-                                </div>
-                                <div class="insidecell">
-                                    <span>$x4</span>
-                                    <span>$x5</span>
-                                    <span>$x6</span>
-                                </div>
-                                <div class="insidecell">
-                                    <span>$x7</span>
-                                    <span>$x8</span>
-                                    <span>$x9</span>
-                                </div>"""              
+            cellString += """<div class="insidecell">
+                                 <span>$x1</span>
+                                 <span>$x2</span>
+                                 <span>$x3</span>
+                             </div>
+                             <div class="insidecell">
+                                 <span>$x4</span>
+                                 <span>$x5</span>
+                                 <span>$x6</span>
+                             </div>
+                             <div class="insidecell">
+                                 <span>$x7</span>
+                                 <span>$x8</span>
+                                 <span>$x9</span>
+                             </div>"""              
             template = Template(cellString)
-            for i in xrange(len(value)):
-                replacement['x' + str(i + 1)] = value[i]
+            for i in xrange(1, 10):
+                if str(i) in value:
+                    replacement['x' + str(i)] = i
             cellString = template.substitute(replacement)    
         else:
             # Dealing with a regular Board so each cell only has a single value
-            cellString +=   """<div class="insidecell">
+            cellString +=   """<div class="">
                                     <span class="single">""" + value + """</span>
                                 </div>"""
         
@@ -133,9 +81,11 @@ class HTMLBoard(Board):
                                 width: 60px;
                                 height: 60px;
                                 text-align: center;
+                                color: black;
                             }
                             
                             div.insidecell {
+                                color: gray;
                             }
                             
                             span.single {
